b03ef65126ae5290af3948bc158c259bc28a157f
sugiyoshi218 <72730557+sugiyoshi218@users.noreply.github.com>
2020-10-25 14:15:41 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,76 +1,80 @@
 'use strict';
 
-const question = document.getElementById('question');
-const choices = document.getElementById('choices');
-const btn = document.getElementById('btn');
-const result = document.getElementById('result');
-const scoreLabel = document.querySelector('#result>p');
+(()=>{
 
-const quizSet=shuffle([
-    {q:'What is A?',c:['A0','A1','A2']},
-    {q:'What is B?',c:['B0','B1','B2']},
-    {q:'What is C?',c:['C0','C1','C2']},
-]);
-let currentNum = 0;
-let isAnswered;
-let score = 0;
+    const question = document.getElementById('question');
+    const choices = document.getElementById('choices');
+    const btn = document.getElementById('btn');
+    const result = document.getElementById('result');
+    const scoreLabel = document.querySelector('#result>p');
 
-function shuffle(arr){
-    for(let i = arr.length-1;i>0;i--){
-        const j = Math.floor(Math.random()*(i+1));
-        [arr[j],arr[i]] = [arr[i],arr[j]];
+    const quizSet=shuffle([
+        {q:'What is A?',c:['A0','A1','A2']},
+        {q:'What is B?',c:['B0','B1','B2']},
+        {q:'What is C?',c:['C0','C1','C2']},
+    ]);
+    let currentNum = 0;
+    let isAnswered;
+    let score = 0;
+
+    function shuffle(arr){
+        for(let i = arr.length-1;i>0;i--){
+            const j = Math.floor(Math.random()*(i+1));
+            [arr[j],arr[i]] = [arr[i],arr[j]];
+        };
+        return arr;
     };
-    return arr;
-};
 
-function checkAnswer(li){
-    if(isAnswered === true){
-        return;
-    };
-    isAnswered = true;
-    if (li.textContent === quizSet[currentNum].c[0]){
-        li.classList.add('correct');
-        score++;
-    }else{
-        li.classList.add('wrong');
+    function checkAnswer(li){
+        if(isAnswered === true){
+            return;
+        };
+        isAnswered = true;
+        if (li.textContent === quizSet[currentNum].c[0]){
+            li.classList.add('correct');
+            score++;
+        }else{
+            li.classList.add('wrong');
+        }
+        btn.classList.remove('disabled');
     }
-    btn.classList.remove('disabled');
-}
 
-function setQuiz(){
-    isAnswered = false;
-    question.textContent = quizSet[currentNum].q;
+    function setQuiz(){
+        isAnswered = false;
+        question.textContent = quizSet[currentNum].q;
 
-    while(choices.firstChild){
-        choices.removeChild(choices.firstChild);
-    }
-    
-    const shuffleChoices = shuffle([...quizSet[currentNum].c]);
-    shuffleChoices.forEach(choice => {
-        const li = document.createElement('li');
-        li.textContent = choice;
-        li.addEventListener('click',()=>{
-            checkAnswer(li);
+        while(choices.firstChild){
+            choices.removeChild(choices.firstChild);
+        }
+
+        const shuffleChoices = shuffle([...quizSet[currentNum].c]);
+        shuffleChoices.forEach(choice => {
+            const li = document.createElement('li');
+            li.textContent = choice;
+            li.addEventListener('click',()=>{
+                checkAnswer(li);
+            });
+            choices.appendChild(li);
         });
-        choices.appendChild(li);
+        if(currentNum === quizSet.length-1){
+            btn.textContent = 'Show Score';
+        }
+    }
+    setQuiz();
+
+    btn.addEventListener('click',()=>{
+        if(btn.classList.contains('disabled')){
+            return;
+        }
+        btn.classList.add('disabled');
+
+        if(currentNum===quizSet.length-1){
+            scoreLabel.textContent = `Score:${score}/${quizSet.length}`
+            result.classList.remove('hidden');
+        }else{
+            currentNum++;
+            setQuiz();
+        };
     });
-    if(currentNum === quizSet.length-1){
-        btn.textContent = 'Show Score';
-    }
-}
-setQuiz();
-
-btn.addEventListener('click',()=>{
-    if(btn.classList.contains('disabled')){
-        return;
-    }
-    btn.classList.add('disabled');
-
-    if(currentNum===quizSet.length-1){
-        scoreLabel.textContent = `Score:${score}/${quizSet.length}`
-        result.classList.remove('hidden');
-    }else{
-        currentNum++;
-        setQuiz();
-    };
-});
+    
+})();
